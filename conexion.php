e9f8cd049b6ab330b76f7de3481bea9cae630b9e
naye23 <naytelyahuara@gmail.com>
2023-04-12 11:31:58 -0600

    version commit

--- conexion.php
+++ conexion.php
@@ -1,10 +1,10 @@
 <?php
 	function conexion(){
-	$host = "host=containers-us-west-141.railway.app";
-	$port = "port=5508";
+	$host = "host=containers-us-west-52.railway.app";
+	$port = "port=5630";
 	$dbname = "dbname=railway";
 	$user = "user=postgres";
-	$password = "password=04ZMZMurwTfxsYFDM5QF";
+	$password = "password=IeyBrA1pevJAMZijU5un";
 	$db = pg_connect("$host $port $dbname $user $password");
     return $db;
 }
